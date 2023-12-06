f5b41db6e0673320e592ea4e2a1f0b56b8a9b5d7
TessierV <5628@holbertonstudents.com>
2023-12-06 00:39:14 -0800

    fixing pep8 and documentation issu

--- markdown2html.py
+++ markdown2html.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
 """ EX: 0 Write a script markdown2html.py that takes an argument 2 strings:
-
 First argument is the name of the Markdown file
 Second argument is the output file name
 Requirements:
-
-If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
-If the Markdown file doesn’t exist: print in STDER Missing <filename> and exit 1
+If the number of arguments is less than 2: print in STDERR Usage:
+./markdown2html.py README.md README.html and exit 1
+If the Markdown file doesn’t exist: print in STDER
+Missing <filename> and exit 1
 Otherwise, print nothing and exit 0 """
 
 import sys
 import os
 import markdown2
+
 
 def convert_markdown_to_html(markdown_file, output_file):
     with open(markdown_file, 'r') as md_file:
@@ -22,9 +23,12 @@ def convert_markdown_to_html(markdown_file, output_file):
     with open(output_file, 'w') as html_file:
         html_file.write(html_content)
 
+
 def main():
+    usage = "Usage: ./markdown2html.py <MarkdownFile> <OutputFile>"
+
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py <MarkdownFile> <OutputFile>", file=sys.stderr)
+        print(usage_message, file=sys.stderr)
         sys.exit(1)
 
     markdown_file = sys.argv[1]
@@ -36,6 +40,7 @@ def main():
 
     convert_markdown_to_html(markdown_file, output_file)
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
