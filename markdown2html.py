f7be9b395bb51af8334d5accb1f6c7d684937f7a
TessierV <5628@holbertonstudents.com>
2023-12-06 00:25:52 -0800

    exo 0 testing

--- markdown2html.py
+++ markdown2html.py
@@ -1,1 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+""" EX: 0 Write a script markdown2html.py that takes an argument 2 strings:
+
+First argument is the name of the Markdown file
+Second argument is the output file name
+Requirements:
+
+If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
+If the Markdown file doesn’t exist: print in STDER Missing <filename> and exit 1
+Otherwise, print nothing and exit 0 """
+
+import sys
+import os
+import markdown2
+
+def convert_markdown_to_html(markdown_file, output_file):
+    with open(markdown_file, 'r') as md_file:
+        markdown_content = md_file.read()
+        html_content = markdown2.markdown(markdown_content)
+
+    with open(output_file, 'w') as html_file:
+        html_file.write(html_content)
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py <MarkdownFile> <OutputFile>", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.exists(markdown_file):
+        print(f"Missing <{markdown_file}>", file=sys.stderr)
+        sys.exit(1)
+
+    convert_markdown_to_html(markdown_file, output_file)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
